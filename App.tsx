b6ea13a8dc28fed79ff2c0210eb112e975d8be88
Pedro Henrique <pedro795027@gmail.com>
2024-03-06 23:53:47 -0300

    feat: alterações de rota

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
-import { StatusBar, Text } from "react-native";
+import {StatusBar, Text} from 'react-native';
 import theme from './src/global/styles/theme';
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import Routes from "./src/routes";
-import { View } from "react-native";
-
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Routes from './src/routes';
+import {View} from 'react-native';
 
 function App() {
-
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{flex: 1}}>
       <StatusBar
         barStyle="light-content"
         backgroundColor={theme.colors.PRIMARY}
       />
       <Routes />
+      <Text>Teste</Text>
     </GestureHandlerRootView>
   );
 }
